5dfa451753aed1dad7a7300552f783de3d31727f
FromICN <sho90427@gmail.com>
2026-08-26 08:51:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
       (수동 편집 시 index.html의 ?v= 와 어긋나 캐시가 영원히 낡은 채로 남는다.)
    ============================================================ */
 
-const CACHE_VERSION = 'v20260824g'; /* @@BUILD:VERSION */
+const CACHE_VERSION = 'v20260825a'; /* @@BUILD:VERSION */
 const STATIC_CACHE  = `tasklog-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `tasklog-runtime-${CACHE_VERSION}`;
 
@@ -23,7 +23,7 @@ const PRECACHE_URLS = [
   './index.html',
   './manifest.json',
   './manifest.json?v=20260810a',
-  './style.css?v=20260824g',
+  './style.css?v=20260825a',
   './profile-modal.css?v=20260722',
   './login.css?v=20260720b',
   './mobile.css?v=20260824a',
@@ -52,7 +52,7 @@ const PRECACHE_URLS = [
   './todo.js?v=20260803a',
   './yearsync.js?v=20260722',
   './script.js?v=20260824g',
-  './pwa.js?v=20260824g',
+  './pwa.js?v=20260825a',
   './mobile-ui.js?v=20260807c',
   './tasklog-icon.svg',
   './favicon-32.png',
